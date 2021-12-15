e9bfbde46f8ffda9cae17f6e518f4ac66745b8b5
ritesh <riteshs50f@gmali.com>
2021-12-16 01:55:31 +0530

    Test commit with New use in clean arch branch

---
+++ app/src/main/java/com/example/musicplayer/domain/usecase/GetAllSongsForFolderUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.musicplayer.domain.usecase
+
+class GetAllSongsForFolderUseCase(folderName:String) {
+
+}
---
+++ app/src/main/java/com/example/musicplayer/domain/usecase/InsertAllSongsUseCase.kt
@@ -0,0 +1,6 @@
+package com.example.musicplayer.domain.usecase
+
+import com.example.musicplayer.data.dao.Song
+
+class InsertAllSongsUseCase(songList: List<Song>) {
+}
